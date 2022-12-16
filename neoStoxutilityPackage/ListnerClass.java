5e5bd2a30d9f85ea9f745dd6cbcaccd9600dcf6b
Jana Pawar <rajnandinipawar456@gmail.com>
2022-12-16 13:53:07 +0530

    master first commit

---
+++ neoStoxutilityPackage/ListnerClass.java
@@ -0,0 +1,36 @@
+package neoStoxutilityPackage;
+
+import java.io.IOException;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+
+import neoStoxBasePackage.BaseClass;
+
+@Listeners(neoStoxutilityPackage.ListnerClass.class)
+public class ListnerClass extends BaseClass implements ITestListener
+ {
+	public void onTestSuccess(ITestResult result) {
+		Reporter.log("TC"+result.getName()+" is completed successfuly",true);
+	}
+
+	
+	public void onTestSkipped(ITestResult result) 
+	{
+		Reporter.log("TC"+result.getName()+" is skipped please check",true);
+			}
+
+	public void onTestFailure(ITestResult result) {
+		Reporter.log("TC "+result.getName()+"is failed",true);
+		try {
+			Utility1.takeScreenshot(driver, result.getName());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
